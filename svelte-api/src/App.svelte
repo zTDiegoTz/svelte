843deb2aafd321187f61c5f0cbc7dd5e9d4728dd
luisdev <luifercuellobeltran@gmail.com>
2024-04-25 21:25:19 -0500

    SVELTE

--- svelte-api/src/App.svelte
+++ svelte-api/src/App.svelte
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script>
   import Character from "./lib/Character.svelte";
 
@@ -26,17 +27,37 @@
   }
 </script>
 
-<main class="container">
-  <h1 class="title">Rick And Morty Svelte</h1>
+<main>
+  <header></header>
+  <h1 class="title">Rick and morty </h1>
+  <hr>
 
-  <div class="btns">
-    <button class="btn"  on:click={paginaanterior} disabled={pagina === 1}>Anterior</button>
-    <button class="btn" on:click={paginasiguiente}>Siguiente</button>
-  </div>
+  <div class="container">
+   
+   
+   
 
-  <div class="grid">
-    {#each personajes as character}
-      <Character {character}/>
-    {/each}
+    <div class="grid">
+      {#each personajes as character}
+        <Character {character}/>
+      {/each}
+    </div>
+
+    <div class="btns">
+      <button class="btn"  on:click={paginaanterior} disabled={pagina === 1}>
+         <i class="fa-solid fa-chevron-left"></i>
+         Anterior
+      </button>
+
+      <button class="btn" on:click={paginasiguiente}>
+        Siguiente
+        <i class="fa-solid fa-chevron-right"></i>
+        
+      </button>
+      
+    </div>
+
   </div>
+  <footer>@Luis Cuelllo</footer>
+  
 </main>
